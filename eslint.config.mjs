7603cfd2a4bf9dfd10640b3872835e61ec5b50e1
shahab khodayar <79662466+sh007kh@users.noreply.github.com>
2025-10-12 21:41:13 +0330

    WIP next store

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,9 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-require-imports": "error",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-vars": "off",
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error",
     },
   },
 ];
