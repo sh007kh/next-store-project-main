ff39275fc82e719ae83d294a3771644bb695ed29
shahab khodayar <79662466+sh007kh@users.noreply.github.com>
2025-12-30 20:09:11 +0330

    bug - "fix  prisma error  "

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,18 @@
-import "dotenv/config";
+import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export { prisma };
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
